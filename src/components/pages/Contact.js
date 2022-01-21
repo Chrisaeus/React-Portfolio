b59d92028f64d73661c9629921ecae1138883950
Chrisaeus <87839602+Chrisaeus@users.noreply.github.com>
2022-01-21 15:01:13 -0800

    renamed projects.json to projectList.json, added template for contact page, ready to implement json

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
 export default function Contact() {
-
+    return (
+        <>
+            <span className="headerPad" id="contactMeScroll"></span>
+            <section className="cardSec" id="contactMe">
+                <aside className="sideTitle">
+                    <h2>Contact Me</h2>
+                </aside>
+                <div class="cardContent">
+                    {/* <h3><a href="mailto:christian.sadler@yahoo.com">christian.sadler@yahoo.com</a></h3>
+                    <h3>206.271.9945</h3>
+                    <h3><a href="https://github.com/Chrisaeus">GitHub</a></h3>
+                    <h3><a href="https://www.linkedin.com/in/christian-sadler-436a3110b/">LinkedIn</a></h3>
+                    <h3><a href="./assets/documents/ChristianSadlerResume.pdf" download>Resume</a></h3> */}
+                </div>
+            </section>
+        </>
+    );
 };
