8a3075b7a2040b6c05c159b818423999740bdecb
Chrisaeus <87839602+Chrisaeus@users.noreply.github.com>
2022-01-21 23:16:46 -0800

    Finished all error functionality on contact form. Doesn't actually submit anything

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,25 +3,52 @@ import React from 'react';
 export default function Contact() {
 
     const formRef = React.useRef(null);
+    const nameRef = React.useRef(null);
+    const nameErrorRef = React.useRef(null);
     const emailRef = React.useRef(null);
-    const errorRef = React.useRef(null);
+    const emailErrorRef = React.useRef(null);
+    const messageRef = React.useRef(null);
+    const messageErrorRef = React.useRef(null);
 
     function validateEmail(email) {
         const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(email).toLowerCase());
     };
 
-    const checkEmail = () => {
-        if (!validateEmail(emailRef.current.value) && emailRef.current.value != "") {
-            errorRef.current.innerText = " Please enter a valid e-mail address.";
+    const checkName = () => {
+        if (!nameRef.current.value) {
+            nameErrorRef.current.innerText = " Please enter a name.";
         } else {
-            errorRef.current.innerText = "";
+            nameErrorRef.current.innerText = "";
+        };
+    };
+
+    const checkEmail = () => {
+        if (!emailRef.current.value) {
+            emailErrorRef.current.innerText = " Please enter an e-mail address.";
+        }else if (!validateEmail(emailRef.current.value) && emailRef.current.value != "") {
+            emailErrorRef.current.innerText = " Please enter a valid e-mail address.";
+        } else {
+            emailErrorRef.current.innerText = "";
+        };
+    };
+
+    const checkMessage = () => {
+        if (!messageRef.current.value) {
+            messageErrorRef.current.innerText = " Please enter a message.";
+        } else {
+            messageErrorRef.current.innerText = "";
         };
     };
 
     const handleFormSubmit = (e) => {
             e.preventDefault();
-            if (validateEmail(emailRef.current.value)) {
+
+            checkName();
+            checkEmail();
+            checkMessage();
+
+            if (nameRef.current.value != "" && validateEmail(emailRef.current.value) && messageRef.current.value != "") {
                 formRef.current.reset();
             }
     };
@@ -36,14 +63,16 @@ export default function Contact() {
                 <div className="cardContent">
                     <form ref={formRef} >
                         <label htmlFor="name"><h3>Name:</h3></label>
-                        <input id="name" />
+                        <input id="name" ref={nameRef} onBlur={checkName} />
+                        <span ref={nameErrorRef}></span>
                         <br/><br/>
-                        <label htmlFor="name"><h3>Email:</h3></label>
+                        <label htmlFor="email"><h3>Email:</h3></label>
                         <input id="email" ref={emailRef} onBlur={checkEmail} />
-                        <span ref={errorRef}></span>
+                        <span ref={emailErrorRef}></span>
                         <br/><br/>
-                        <label htmlFor="name"><h3>Message:</h3></label>
-                        <textarea id="message"/>
+                        <label htmlFor="message"><h3>Message:</h3></label>
+                        <textarea id="message" ref={messageRef} onBlur={checkMessage} />
+                        <span ref={messageErrorRef}></span>
                         <br/><br/>
                         <button type="button" onClick={handleFormSubmit}>
                             Submit
